38003749be085aa5765ddadaac42924689c4b2ac
Jubilant Agida <jubilantagida@gmail.com>
2023-02-16 16:05:05 -0800

    sisxth task on C.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main- this program prints a string followed by a new line
- *Returns: 0
+ *Return:0
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
