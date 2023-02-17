2c3aba92a633889ec704570ad1c9b5442274291a
Jubilant Agida <jubilantagida@gmail.com>
2023-02-16 16:39:45 -0800

    ninth task on C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *main- this program prints a string to the standard error
+ *Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
