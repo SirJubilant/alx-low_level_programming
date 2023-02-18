447781b7e6e8777dc3d2783e8a029e4eceb070cd
Jubilant Agida <jubilantagida@gmail.com>
2023-02-17 17:40:38 -0800

    advanced tasks on variables and conditionals

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - is entry of program
  *
- * Description: Prints all possible different combinations of two digits.
- * Numbers must be separated by ", " followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function (every other function is forbidden)
- * You can only use putchar five times maximum in your code
- * You are not allowed to use any variable of type char
- *
- * Return: Always 0 (success)
+ * Return: 0, if succesful
  */
 int main(void)
 {
-    int i = 0;
-
-    while (i < 100)
-    {
-        if (i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
-    }
-
-    putchar('\n');
-    return (0);
+	/*
+	 * prints all single integers, with comma and space after
+	 * loops through 0-9, prints them,prints comma,print space,
+	 * use only putchar
+	 */
+	int x;
+for (x = 0 ; x < 10 ; x++)
+	{
+		putchar((x % 10) + '0');
+		if (x == 9)
+		/*continue printing comma and space except if x == 9*/
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
-
 
