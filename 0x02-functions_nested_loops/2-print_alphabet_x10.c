46826e718306d59a3471b67f9dbdb1f019170472
Jubilant Agida <jubilantagida@gmail.com>
2023-02-21 17:13:19 -0800

    third task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - uses nested loops to print lower case and new line
+ */
+void print_alphabet_x10(void)
+{
+int newline = 0;
+char = low;
+
+while (newline < 10)
+{
+for (low = 'a'; low <= 'z'; low++)
+{
+_putchar(low);
+}
+_putchar('\n');
+newline++;
+}
+}
