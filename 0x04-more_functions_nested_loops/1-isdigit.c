1f0cca764e82064b0a8e8bcb0e3a79a3c42303c0
Jubilant Agida <jubilantagida@gmail.com>
2023-02-23 16:09:17 -0800

    second task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - checks if c is a digit
  * @c: the value to be checked
- * Return: 1 - if c is a digit 0 - if otherwise
+ * Return: 1 - if c is a digit, 0 - if otherwise
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
