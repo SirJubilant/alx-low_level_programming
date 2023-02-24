d7383a6e7f9169562850bfba73c101b3af9a20ee
Jubilant Agida <jubilantagida@gmail.com>
2023-02-23 16:23:02 -0800

    third task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - this function multiplies 2 given integers
+ * @a: an integer to be multiplied
+ * @b: another integer to be multiplied
+ * Return: result of the multiplication.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
