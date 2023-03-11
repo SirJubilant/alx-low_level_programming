ab3aaccc995fb6bd27467c8669476c0c94ae0ad6
Jubilant Agida <jubilantagida@gmail.com>
2023-03-10 18:03:22 -0800

    first task on argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints the program's name, followed by a new line
+ * @argc: Argument counter
+ * @argv: Argument vector, array of pointers to the argument
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This file contains tasks on argc and argv
